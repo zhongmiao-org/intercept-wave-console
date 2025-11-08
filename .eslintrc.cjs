c3f3d14705bca4acce258ce695da213b63e0595c
ark-65 <liuwufangzhou@vip.qq.com>
2025-11-08 14:36:20 +0800

    ci: add ci

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,7 +17,21 @@ module.exports = {
   rules: {
     'vue/multi-word-component-names': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_', caughtErrorsIgnorePattern: '^_' }
+    ],
+    '@typescript-eslint/ban-types': 'off',
+    'prefer-const': 'off',
   },
+  overrides: [
+    {
+      files: ['*.d.ts'],
+      rules: {
+        '@typescript-eslint/no-unused-vars': 'off',
+        '@typescript-eslint/ban-types': 'off',
+      },
+    },
+  ],
   ignorePatterns: ['dist/', 'node_modules/'],
 };
-
